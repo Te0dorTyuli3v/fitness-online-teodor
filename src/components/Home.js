a1fa207af00ebe9ee60def6aa4a98b928f28faa9
Teodor <tyuliev.teodor@gmail.com>
2024-11-07 23:41:41 +0200

    Добавяне на изображение с упражнения за начинаещи в началната страница

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 // src/components/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import exerciseImage from '../assets/exercises-for-beginners.jpg'; // Импортирай изображението
 
 function Home() {
   return (
@@ -8,10 +9,17 @@ function Home() {
       <h2>Добре дошли във "Фитнес Онлайн Теодор"</h2>
       <p>Това приложение ще ви помогне да следите и организирате тренировките си. Можете да добавяте тренировки, като попълните името, описанието и продължителността на всяка тренировка.</p>
       <p>Започнете, като добавите първата си тренировка, или разгледайте списъка с текущи тренировки.</p>
+
       <div>
         <Link to="/add">
           <button>Добави Нова Тренировка</button>
         </Link>
+      </div>
+
+      {/* Добавяме изображението със стилове */}
+      <div style={{ marginTop: '20px', textAlign: 'center' }}>
+        <h3>Упражнения за Начинаещи</h3>
+        <img src={exerciseImage} alt="Упражнения за начинаещи" style={{ maxWidth: '100%', height: 'auto', borderRadius: '8px' }} />
       </div>
     </div>
   );
